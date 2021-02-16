8cd8c054142f9aa191ad06f3a79dd81c621cd6af
Orestis Markou <orestis@orestis.gr>
2021-02-16 09:03:02 +0100

    added iterm and fish homebrew for big sur

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,8 @@ if not status --is-interactive
     exit
 end
 
+test -e /opt/homebrew/bin/brew; and eval (/opt/homebrew/bin/brew shellenv)
+
 source (brew --prefix asdf)/asdf.fish
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
