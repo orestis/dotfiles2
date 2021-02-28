a03f7ca384d25f80a8955a837ddd63fe1fcf09f4
Orestis Markou <orestis@orestis.gr>
2021-02-28 11:32:48 +0100

    Updates to fish and neovim

--- fish/config.fish
+++ fish/config.fish
@@ -8,5 +8,11 @@ test -e /opt/homebrew/bin/brew; and eval (/opt/homebrew/bin/brew shellenv)
 
 source (brew --prefix asdf)/asdf.fish
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+test -e ~/dev/nosco/mongo/4.0.22; and set PATH $PATH ~/dev/nosco/mongo/4.0.22/bin;
+test -e /Applications/Postgres.app/; and set PATH $PATH /Applications/Postgres.app/Contents/Versions/latest/bin;
+
+test -e ~/.ebcli-virtual-env/executables; and set PATH $PATH ~/.ebcli-virtual-env/executables;
+test -e ~/.pyenv/versions/3.9.1/bin; and set PATH $PATH ~/.pyenv/versions/3.9.1/bin;
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish;
 
